f09c4c0b08ae598d0487f7414fc504c977a4f84d
Timofey <nikiforov.ta@edu.spbstu.ru>
2020-10-13 14:59:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab3task3/SecondActivity.kt
@@ -0,0 +1,75 @@
+package com.example.lab3task3
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val toolbar: Toolbar = findViewById(R.id.stoolbar)
+        setSupportActionBar(toolbar)
+
+        val drawer: DrawerLayout = findViewById(R.id.sdrawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navView: NavigationView = findViewById(R.id.snav_view)
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.snav_about -> {
+                    val drawerL: DrawerLayout = findViewById(R.id.sdrawer_layout)
+                    drawerL.closeDrawer(GravityCompat.START)
+
+                    startActivity(Intent(this, AboutActivity::class.java))
+                    true
+                }
+                R.id.snav_first -> {
+                    val drawerL: DrawerLayout = findViewById(R.id.sdrawer_layout)
+                    drawerL.closeDrawer(GravityCompat.START)
+
+                    intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+        navView.bringToFront()
+    }
+
+    fun onClick21(view: View) {
+        finish()
+    }
+
+    override fun onBackPressed() {
+        val drawer: DrawerLayout = findViewById(R.id.sdrawer_layout)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    fun onClick23(view: View) {
+        startActivity(Intent(this, ThirdActivity::class.java))
+    }
+}
